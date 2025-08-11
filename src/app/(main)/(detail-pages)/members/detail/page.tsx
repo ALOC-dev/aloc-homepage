9ef8e474a0070a1b79bd04952fd2a8436f8e2506
song9252 <song925252@gmail.com>
2025-08-11 20:51:08 +0900

    feat: 멤버 디테일 페이지 구현

---
+++ src/app/(main)/(detail-pages)/members/detail/page.tsx
@@ -0,0 +1,91 @@
+import Image from 'next/image';
+
+export default function MemberDetail() {
+  return (
+    <div className='relative w-full h-full bg-[#FFFFFF]'>
+      {/* 상단 헤더 바 */}
+      <div className='w-full h-[63.1px] bg-[#E0DFE4] relative shadow-[0_2px_4px_rgba(0,0,0,0.05)]'>
+        {/* macOS 트래픽 라이트 */}
+        <div className='absolute top-[18.03px] left-[15.57px] flex space-x-[14.33px]'>
+          <div className='w-[24.58px] h-[24.58px] bg-[#FF5F56] rounded-full' />
+          <div className='w-[24.58px] h-[24.58px] bg-[#FDBC2E] rounded-full' />
+          <div className='w-[24.58px] h-[24.58px] bg-[#28C83E] rounded-full' />
+        </div>
+
+        {/* 페이지 타이틀 */}
+        <h1 className='absolute top-[5px] left-1/2 -translate-x-1/2 text-[20.49px] font-[700] text-black center'>
+          3기 송희영
+        </h1>
+      </div>
+
+      {/* 좌측 패널 */}
+      <div className='absolute bottom-0 w-[289.89px]'>
+        {/* 프로필 이미지 */}
+        <div className='absolute left-[40.97px] top-[32.76px] w-[196.67px] h-[196.67px] rounded-[19.67px] overflow-hidden shadow-[0_1px_3px_rgba(0,0,0,0.1)]'>
+          <Image
+            src='/images/members/detail-icon.png'
+            alt='프로필 이미지'
+            fill
+            className='object-cover'
+            sizes='(max-width: 281px) 196px, 196px'
+          />
+        </div>
+
+        {/* 역할 배지 */}
+        <div className='absolute left-[44.25px] top-[281.56px]'>
+          <span className='text-[21.31px] font-[700] text-[#408CFF]'>부원</span>
+        </div>
+
+        {/* 이름 */}
+        <div className='absolute left-[44.25px] top-[320.07px]'>
+          <span className='text-[40.97px] font-[700] text-[#3C414C]'>
+            송희영
+          </span>
+        </div>
+
+        {/* 태그(직무/관심분야) */}
+        <div className='absolute left-[44.25px] top-[382.35px] flex gap-[8.19px]'>
+          <span className='px-[8.19px] py-[5.74px] rounded-[9.83px] border border-[#A9ADB9] text-[13.93px] font-[700] text-black'>
+            프론트엔드
+          </span>
+          <span className='px-[8.19px] py-[5.74px] rounded-[9.83px] border border-[#A9ADB9] text-[13.93px] font-[700] text-black'>
+            백엔드
+          </span>
+          <span className='px-[8.19px] py-[5.74px] rounded-[9.83px] border border-[#A9ADB9] text-[13.93px] font-[700] text-black'>
+            AI
+          </span>
+        </div>
+
+        {/* GitHub 아이콘 버튼 */}
+        <a
+          href='https://github.com/'
+          target='_blank'
+          rel='noreferrer noopener'
+          aria-label='GitHub 프로필'
+          className='absolute left-[44.25px] top-[432.33px] w-[73.76px] h-[73.76px]'
+        >
+          <Image
+            src='/images/members/profile-icon.png'
+            alt='GitHub'
+            width={44}
+            height={44}
+            className='object-contain'
+          />
+        </a>
+      </div>
+
+      {/* 우측 콘텐츠 */}
+      <div className='absolute left-[281.89px] right-0 top-[63.1px] h-full border-l border-[#A9ADB9]/70'>
+        {/* 내부 패딩과 타이포 */}
+        <div className='relative w-full h-full px-[60px] pt-[30.16px]'>
+          <h2 className='text-[#3C414C] text-[32.78px] font-[700] leading-[1.2] mb-[24px]'>
+            자기소개
+          </h2>
+          <p className='text-[#5C5E66] text-[24.58px] leading-[1.2] max-w-[573px]'>
+            안녕하세요. 서울시립대학교 컴퓨터과학부 송희영입니다.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
