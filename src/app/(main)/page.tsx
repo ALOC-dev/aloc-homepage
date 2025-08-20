c07515b88746afd9e738405f91b3d833ac8ac824
song9252 <song925252@gmail.com>
2025-08-21 01:22:08 +0900

    style : 메인에 맥북 직접 드로잉

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link';
 
 export default function Main() {
   return (
-    <div className='relative min-w-[1440px] min-h-[980px] bg-white overflow-hidden flex items-center justify-center'>
+    <div className='relative min-w-[1440px] min-h-[1080px] bg-white overflow-hidden flex items-center justify-center'>
       {/* 로고 아이콘 */}
       <div className='absolute left-[1.1%] top-[3%] z-20'>
         <Image
@@ -23,19 +23,19 @@ export default function Main() {
         </button>
       </div>
       {/* 배경 이미지와 스티커들을 포함하는 컨테이너 */}
-      <div className='w-[1008px] h-[756px] relative'>
-        {/* 배경 이미지 */}
-        <Image
-          src='/images/main/macbook.png'
-          alt='Background'
-          width={1440}
-          height={1080}
-          className='absolute inset-0 w-full h-full'
-          priority
-        />
-
+      <div className='w-[1188px] relative aspect-[2054/1377] rounded-4xl bg-gradient-to-t from-gray-800 via-gray-700 to-gray-600 shadow-2xl'>
+        {/* 메인 로고 - 정중앙 */}
+        <div className='absolute inset-0 flex items-center justify-center z-5'>
+          <Image
+            src='/images/main/logo.png'
+            alt='ALOC Main Logo'
+            width={150}
+            height={150}
+            className='object-contain'
+          />
+        </div>
         {/* 스티커 1 - activity 스티커 */}
-        <div className='absolute left-[5.75%] top-[28.5%] transform origin-top-left -rotate-[17.14deg] z-10 w-[400px] aspect-[1150/396]'>
+        <div className='absolute left-[5.75%] top-[18.5%] transform origin-top-left -rotate-[17.14deg] z-10 w-[400px] aspect-[1150/396]'>
           <Link href='/activity'>
             <Image
               src='/images/main/activity.png'
@@ -47,7 +47,7 @@ export default function Main() {
         </div>
 
         {/* 스티커 2 - camera 스티커 */}
-        <div className='absolute left-[70%] top-[22.5%]  origin-top-left transform rotate-[9.52deg] z-10 w-[300px] aspect-[464/696]'>
+        <div className='absolute left-[70%] top-[32.5%]  origin-top-left transform rotate-[9.52deg] z-10 w-[300px] aspect-[464/696]'>
           <Link href='/gallery'>
             <Image
               src='/images/main/camera.png'
@@ -71,7 +71,7 @@ export default function Main() {
         </div>
 
         {/* 스티커 4 - computer-science 스티커 */}
-        <div className='absolute left-[55%] top-[15%] transform origin-top-left rotate-[5deg] z-10 w-[380px] aspect-[740/302]'>
+        <div className='absolute left-[15%] top-[38%] transform origin-top-left rotate-[5deg] z-10 w-[280px] aspect-[740/302]'>
           <Image
             src='/images/main/computer_science.png'
             alt='Computer Science Sticker'
@@ -101,7 +101,7 @@ export default function Main() {
         </div>
 
         {/* 스티커 7 - aloc */}
-        <div className='absolute left-[35.1%] top-[31.7%] transform origin-top-left rotate-[9.59deg] z-10 w-[352px] aspect-[467/375]'>
+        <div className='absolute left-[62.1%] top-[-4.7%] transform origin-top-left rotate-[9.59deg] z-10 w-[452px] aspect-[467/375]'>
           <Link href='/introduction'>
             <Image
               src='/images/main/aloc.png'
