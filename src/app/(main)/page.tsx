ff657aeaadbde5c080a876fb86f2ec47e5ed942f
song9252 <song925252@gmail.com>
2025-08-26 00:19:10 +0900

    feat : main - 스티커 요소 상대 크기로 설정

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link';
 
 export default function Main() {
   return (
-    <div className='relative min-w-[1440px] min-h-[1080px] bg-white overflow-hidden flex items-center justify-center'>
+    <div className='fixed w-screen h-screen bg-white overflow-hidden flex items-center justify-center'>
       {/* 로고 아이콘 */}
       <div className='absolute left-[1.1%] top-[3%] z-20'>
         <Image
@@ -23,19 +23,18 @@ export default function Main() {
         </button>
       </div>
       {/* 배경 이미지와 스티커들을 포함하는 컨테이너 */}
-      <div className='w-[1188px] relative aspect-[2054/1377] rounded-4xl bg-gradient-to-t from-gray-800 via-gray-700 to-gray-600 shadow-2xl'>
+      <div className='w-[1088px] relative aspect-[2054/1377] rounded-4xl bg-gradient-to-t from-gray-800 via-gray-700 to-gray-600 shadow-2xl'>
         {/* 메인 로고 - 정중앙 */}
-        <div className='absolute inset-0 flex items-center justify-center z-5'>
+        <div className='absolute inset-0 left-[50%] top-[50%] -translate-x-1/2 -translate-y-1/2 w-[15%] aspect-[1/1] z-5'>
           <Image
             src='/images/main/logo.png'
             alt='ALOC Main Logo'
-            width={150}
-            height={150}
+            fill
             className='object-contain'
           />
         </div>
         {/* 스티커 1 - activity 스티커 */}
-        <div className='absolute left-[5.75%] top-[18.5%] transform origin-top-left -rotate-[17.14deg] z-10 w-[400px] aspect-[1150/396]'>
+        <div className='absolute left-[5.75%] top-[18.5%] transform origin-top-left -rotate-[17.14deg] z-10 w-[36.8%] aspect-[1150/396]'>
           <Link href='/activity'>
             <Image
               src='/images/main/activity.png'
@@ -47,7 +46,7 @@ export default function Main() {
         </div>
 
         {/* 스티커 2 - camera 스티커 */}
-        <div className='absolute left-[70%] top-[32.5%]  origin-top-left transform rotate-[9.52deg] z-10 w-[300px] aspect-[464/696]'>
+        <div className='absolute left-[70%] top-[32.5%]  origin-top-left transform rotate-[9.52deg] z-10 w-[27.6%] aspect-[464/696]'>
           <Link href='/gallery'>
             <Image
               src='/images/main/camera.png'
@@ -59,7 +58,7 @@ export default function Main() {
         </div>
 
         {/* 스티커 3 - 맥주잔 스티커 */}
-        <div className='absolute left-[7.9%] top-[50.8%] transform origin-top-left rotate-[10.61deg] z-10 w-[320px] aspect-[1/1]'>
+        <div className='absolute left-[7.9%] top-[50.8%] transform origin-top-left rotate-[10.61deg] z-10 w-[29.4%] aspect-[1/1]'>
           <Link href='/members'>
             <Image
               src='/images/main/맥주잔.png'
@@ -71,7 +70,7 @@ export default function Main() {
         </div>
 
         {/* 스티커 4 - computer-science 스티커 */}
-        <div className='absolute left-[15%] top-[38%] transform origin-top-left rotate-[5deg] z-10 w-[280px] aspect-[740/302]'>
+        <div className='absolute left-[15%] top-[38%] transform origin-top-left rotate-[5deg] z-10 w-[25.7%] aspect-[740/302]'>
           <Image
             src='/images/main/computer_science.png'
             alt='Computer Science Sticker'
@@ -81,7 +80,7 @@ export default function Main() {
         </div>
 
         {/* 스티커 5 - 부엉이 캐릭터 */}
-        <div className='absolute left-[53.1%] top-[58.8%] -rotate-[10deg] z-10 w-[144px] aspect-[782/1173]'>
+        <div className='absolute left-[53.1%] top-[58.8%] -rotate-[10deg] z-10 w-[13.2%] aspect-[782/1173]'>
           <Image
             src='/images/main/시립대마스코트.png'
             alt='University Mascot'
@@ -91,7 +90,7 @@ export default function Main() {
         </div>
 
         {/* 스티커 6 - 서울시립대 로고 */}
-        <div className='absolute left-[48.3%] top-[78%] z-10 w-[400px] aspect-[347/152]'>
+        <div className='absolute left-[48.3%] top-[78%] z-10 w-[36.8%] aspect-[347/152]'>
           <Image
             src='/images/main/서울시립대로고.png'
             alt='University of Seoul Logo'
@@ -101,7 +100,7 @@ export default function Main() {
         </div>
 
         {/* 스티커 7 - aloc */}
-        <div className='absolute left-[62.1%] top-[-4.7%] transform origin-top-left rotate-[9.59deg] z-10 w-[452px] aspect-[467/375]'>
+        <div className='absolute left-[62.1%] top-[-4.7%] transform origin-top-left rotate-[9.59deg] z-10 w-[41.5%] aspect-[467/375]'>
           <Link href='/introduction'>
             <Image
               src='/images/main/aloc.png'
