69be045e7f32a525c1af8c35d4eb33e93bd74471
2fill <ichaeu@naver.com>
2025-09-01 14:30:40 +0900

    style: main 스티커 재배치

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -32,7 +32,7 @@ export default function Main() {
   return (
     <div
       className='fixed w-screen h-screen bg-cover bg-center overflow-hidden'
-      style={{ backgroundImage: "url('/images/main/main-background.png')" }}
+      style={{ backgroundImage: "url('/images/main/main-background-2.png')" }}
     >
       {/* 헤더 영역 - flexbox 밖으로 분리 */}
       <div className='absolute top-0 left-0 right-0 h-[10%] z-20'>
@@ -47,8 +47,8 @@ export default function Main() {
       {/* 맥북 컨테이너 - 중앙 정렬 */}
       <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 relative'
         style={{
-          width: "600px",
-          height: "400px",
+          width: "750px",
+          height: "500px",
           background: "linear-gradient(113.96deg, #E6E5E6 0%, #35302B 100%)",
           boxShadow: "30px 30px 50px rgba(0, 0, 0, 0.8)",
           borderRadius: "30px",
@@ -65,66 +65,67 @@ export default function Main() {
             className='object-contain'
           />
         </div>
-        {/* 스티커 1 - activity 스티커 */}
+
+        {/* 스티커 1 - about 스티커 */}
         <div
-          className='absolute left-[5.75%] top-[18.5%] transform origin-top-left -rotate-[17.14deg] z-10 w-[36.8%] aspect-[1150/396]'
+          className='absolute right-[40.5%] top-[25%] transform origin-top-left -rotate-[17.14deg] z-10 w-[80%] aspect-[1150/396]'
+          onMouseEnter={() => handleStickerHover('activity')}
+          onMouseLeave={handleStickerLeave}
+        >
+          <Link href='/instroduction'>
+            <Image
+              src='/images/main/about.png'
+              alt='About Sticker'
+              fill
+              className={getStickerClassName('about')}
+            />
+          </Link>
+        </div>
+
+        {/* 스티커 2 - activity */}
+        <div
+          className='absolute left-[65%] top-[2%] transform origin-top-left rotate-[9.59deg] z-10 w-[30%] aspect-[467/375]'
           onMouseEnter={() => handleStickerHover('activity')}
           onMouseLeave={handleStickerLeave}
         >
           <Link href='/activity'>
             <Image
-              src='/images/main/activity.png'
-              alt='Activity Sticker'
+              src='/images/main/activity-2.png'
+              alt='Activity'
               fill
               className={getStickerClassName('activity')}
             />
           </Link>
         </div>
 
-        {/* 스티커 2 - camera 스티커 */}
+        {/* 스티커 3 - 멤버 스티커 */}
         <div
-          className='absolute left-[70%] top-[32.5%]  origin-top-left transform rotate-[9.52deg] z-10 w-[27.6%] aspect-[464/696]'
+          className='absolute left-[7.0%] top-[36%] transform origin-top-left rotate-[10.61deg] z-10 w-[45%] aspect-[1/1]'
+          onMouseEnter={() => handleStickerHover('member')}
+          onMouseLeave={handleStickerLeave}
+        >
+          <Link href='/members'>
+            <Image
+              src='/images/main/member.png'
+              alt='Member Sticker'
+              fill
+              className={getStickerClassName('member')}
+            />
+          </Link>
+        </div>
+
+        {/* 스티커 4 - camera 스티커 */}
+        <div
+          className='absolute left-[62%] top-[45%]  origin-top-left transform rotate-[-9deg] z-10 w-[30%] aspect-[464/696]'
           onMouseEnter={() => handleStickerHover('camera')}
           onMouseLeave={handleStickerLeave}
         >
           <Link href='/gallery'>
             <Image
-              src='/images/main/camera.png'
+              src='/images/main/camera-2.png'
               alt='Camera Sticker'
               fill
               className={getStickerClassName('camera')}
-            />
-          </Link>
-        </div>
-
-        {/* 스티커 3 - 맥주잔 스티커 */}
-        <div
-          className='absolute left-[7.9%] top-[50.8%] transform origin-top-left rotate-[10.61deg] z-10 w-[29.4%] aspect-[1/1]'
-          onMouseEnter={() => handleStickerHover('beer')}
-          onMouseLeave={handleStickerLeave}
-        >
-          <Link href='/members'>
-            <Image
-              src='/images/main/맥주잔.png'
-              alt='Beer Sticker'
-              fill
-              className={getStickerClassName('beer')}
-            />
-          </Link>
-        </div>
-
-        {/* 스티커 7 - aloc */}
-        <div
-          className='absolute left-[62.1%] top-[-4.7%] transform origin-top-left rotate-[9.59deg] z-10 w-[41.5%] aspect-[467/375]'
-          onMouseEnter={() => handleStickerHover('aloc')}
-          onMouseLeave={handleStickerLeave}
-        >
-          <Link href='/introduction'>
-            <Image
-              src='/images/main/aloc.png'
-              alt='ALL LINKED ONE CODE'
-              fill
-              className={getStickerClassName('aloc')}
             />
           </Link>
         </div>
